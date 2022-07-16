c4375f903a34f2872587c65c77ee0d76a53e6e2a
vicente2001-admin <108435811+vicente2001-admin@users.noreply.github.com>
2022-07-16 17:44:20 -0500

    Add files via upload

---
+++ plugins/adult-videoxxx.js
@@ -0,0 +1,15 @@
+let handler  = async (m, { conn, usedPrefix, command }) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'   
+let res = pickRandom(asupan)
+conn.sendHydrated(m.chat, null, null, res, null, null, null, null, [['◀️𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴▶️', `/${command}`]], m)}
+handler.help = ['videoxxx']
+handler.tags = ['random']
+handler.command = /^videos|vídeos$/i
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(list.length * Math.random())]}
+
+const asupan = [
+
+]
